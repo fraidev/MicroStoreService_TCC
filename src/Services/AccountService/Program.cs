da6dc8adaf1014b970a1b139cdaba93bfad6cd46
Felipe Cardozo <fraifelipe@gmail.com>
2018-10-10 21:26:53 -0300

    Refatorando diretorios e projetos

--- src/Services/AccountService/Program.cs
+++ src/Services/AccountService/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace Auth
+namespace AccountService
 {
     public class Program
     {
